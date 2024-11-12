03b44d64666c751ac2743295b4a85f0b3f904fba
apaza3000 <96398557+apaza3000@users.noreply.github.com>
2024-11-12 07:38:37 -0500

    agregando la clase y metodo para consulta de ruc

---
+++ TpdDiurno/servicios/DataRucApiDev.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpdDiurno.servicios
+{
+    public class DataRucApiDev
+    {
+        string ruc, direccion, direccion_completa, nombre_o_razon_social, estado, condicion, distrito,provincia,departamento, ubigeo_sunat;
+
+        public string Ruc { get => ruc; set => ruc = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Direccion_completa { get => direccion_completa; set => direccion_completa = value; }
+        public string Nombre_o_razon_social { get => nombre_o_razon_social; set => nombre_o_razon_social = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Condicion { get => condicion; set => condicion = value; }
+        public string Distrito { get => distrito; set => distrito = value; }
+        public string Provincia { get => provincia; set => provincia = value; }
+        public string Departamento { get => departamento; set => departamento = value; }
+        public string Ubigeo_sunat { get => ubigeo_sunat; set => ubigeo_sunat = value; }
+    }
+}
--- TpdDiurno/servicios/ServiceApiDev.cs
+++ TpdDiurno/servicios/ServiceApiDev.cs
@@ -27,14 +27,41 @@ namespace TpdDiurno.servicios
         public async Task<DataDniApiDev> getByDni(string nrodni)
         {
             var datos = new { dni = nrodni };
-            string json=JsonConvert.SerializeObject(datos);
-            HttpContent content = new StringContent(json,Encoding.UTF8,"application/json");
+            string json = JsonConvert.SerializeObject(datos);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
             HttpResponseMessage respuesta = await http.PostAsync("dni", content);
             if (respuesta.IsSuccessStatusCode)
             {
                 string body = await respuesta.Content.ReadAsStringAsync();
                 var resbody = JsonConvert.
                     DeserializeObject<ResponseApiDev<DataDniApiDev>>(body);
+
+                if (resbody.Success)
+                {
+                    return resbody.Data;
+                }
+                else
+                {
+                    throw new Exception("no hay datos");
+                }
+            }
+            else
+            {
+                throw new Exception("La solicitud no se concreto");
+            }
+
+        } 
+        public async Task<DataRucApiDev> getByRuc(string numeroruc)
+        {
+            var datos = new { ruc = numeroruc };
+            string json = JsonConvert.SerializeObject(datos);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage respuesta = await http.PostAsync("ruc", content);
+            if (respuesta.IsSuccessStatusCode)
+            {
+                string body = await respuesta.Content.ReadAsStringAsync();
+                var resbody = JsonConvert.
+                    DeserializeObject<ResponseApiDev<DataRucApiDev>>(body);
 
                 if (resbody.Success)
                 {
